02b9b26792fbec409e635d67df024daa33ed6ff0
skkhadka <skkhadka@skkhadka-HP-EliteBook-8470w.(none)>
2013-06-06 16:54:55 +0545

    Created new project with latest rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerProject::Application.config.secret_token = '4760c22a77a3c5246bda1616c2e76737f2a6c9771622275bc4514e6f67f642091a9c8822633a6121719e68d4cde58aac3a6b62665099ebef3ea9bab59eb7c49f'
